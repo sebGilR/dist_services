a5af2497ce4c274d49bc2e061b3d3fc433f24a72
Sebastian Gil <lbuggie.21@gmail.com>
2021-10-22 16:58:06 -0500

    Create log and service packages

---
+++ cmd/logservice/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	stlog "log"
+
+	"github.com/sebGilR/dist_services/log"
+	"github.com/sebGilR/dist_services/service"
+)
+
+func main() {
+	log.Run("./app.log")
+
+	host, port := "localhost", "4000"
+
+	ctx, err := service.Start(
+		context.Background(),
+		"Log Service",
+		host,
+		port, log.RegisterHandlers,
+	)
+
+	if err != nil {
+		stlog.Fatal(err)
+	}
+
+	<-ctx.Done()
+	fmt.Println("Shutting down log service")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sebGilR/dist_services
+
+go 1.17
